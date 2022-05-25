661e575578c3e261775bc10b273d19f8bc43cdd4
devon-n <devnathan94@gmail.com>
2022-05-25 14:57:02 +1000

    assigned arrow function

--- pages/api/services.ts
+++ pages/api/services.ts
@@ -1,9 +1,11 @@
-// import { NextApiRequest, NextApiResponse } from "next"
+import { NextApiRequest, NextApiResponse } from "next"
 
-// import { services } from '../../data'
+import { services } from '../../data'
 
-// export default (req:NextApiRequest, res:NextApiResponse) => {
+const getServices = (req:NextApiRequest, res:NextApiResponse) => {
 
-//     console.log(services)
-//     res.status(200).json({services})
-// }
+    console.log(services)
+    res.status(200).json({services})
+}
+
+export default getServices
